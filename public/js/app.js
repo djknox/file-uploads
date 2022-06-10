16223552a85b75bde7d46afda6acb40873426fca
DJ Knox <dhknox@gmail.com>
2022-06-10 20:06:10 +0000

    add CSRF token to Filepond

--- public/js/app.js
+++ public/js/app.js
@@ -19593,6 +19593,16 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+(0,vue_filepond__WEBPACK_IMPORTED_MODULE_0__.setOptions)({
+  server: {
+    process: {
+      url: './upload',
+      headers: {
+        'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf_token"]').content
+      }
+    }
+  }
+});
 var FilePond = vue_filepond__WEBPACK_IMPORTED_MODULE_0___default()((filepond_plugin_file_validate_type__WEBPACK_IMPORTED_MODULE_2___default()));
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   components: {
@@ -19639,7 +19649,6 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     name: "image",
     ref: "pond",
     "label-idle": "Click to choose image, or drag here...",
-    server: "/upload",
     "accepted-file-types": "image/*"
   }, null, 512
   /* NEED_PATCH */
--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf_token" content="{{ csrf_token() }}">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <title>File Uploads</title>
