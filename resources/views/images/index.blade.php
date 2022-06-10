723ca556d6bff7c99f288d22a05c51a0d2cc138d
DJ Knox <dhknox@gmail.com>
2022-06-10 17:35:51 +0000

    install tailwindcss

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <title>File Uploads</title>
 
@@ -16,6 +17,6 @@
         </style>
     </head>
     <body>
-      
+
     </body>
 </html>
