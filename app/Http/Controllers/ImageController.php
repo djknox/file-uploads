afe709fb666135c980d31ea5ecb3a73cbc50566c
DJ Knox <dhknox@gmail.com>
2022-06-10 20:05:53 +0000

    add logic to ImageController@store to validate, save, and return an image record

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Image;
 
 class ImageController extends Controller
 {
@@ -17,5 +18,32 @@ class ImageController extends Controller
 
     public function store(Request $request)
     {
+        // validate incoming file
+        if (!$request->hasFile('image')) {
+            return response()->json(['error' => 'There is no image present.'], 400);
+        }
+
+        $request->validate([
+            'image' => 'required|file|image',
+        ]);
+
+        // save the file in storage
+        $path = $request->file('image')->store('public/images');
+
+        if (!$path) {
+            return response()->json(['error' => 'The file could not be saved.'], 500);
+        }
+
+        // create an image record
+        $uploadedFile = $request->file('image');
+
+        $image = Image::create([
+            'name' => $uploadedFile->hashName(),
+            'extension' => $uploadedFile->extension(),
+            'size' => $uploadedFile->getSize(),
+        ]);
+
+        // return the image record back to the UI
+        return $image;
     }
 }
