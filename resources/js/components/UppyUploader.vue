63efdb73d8b692c5e17ea36d10300f90b7c650f9
DJ Knox <dhknox@gmail.com>
2022-06-13 16:07:18 +0000

    install Uppy npm packages and implement uploader in UppyUploader component

--- resources/js/components/UppyUploader.vue
+++ resources/js/components/UppyUploader.vue
@@ -2,9 +2,38 @@
     <div class="mt-8">
         <h2 class="text-xl">Uppy</h2>
         
+        <dashboard
+          :uppy="uppy"
+          :props="{
+            theme: 'light',
+            proudlyDisplayPoweredByUppy: false,
+          }"
+        />
     </div>
 </template>
 <script>
+import Uppy from '@uppy/core'
+import XHRUpload from '@uppy/xhr-upload'
+import { Dashboard } from '@uppy/vue'
+
+import '@uppy/core/dist/style.css'
+import '@uppy/dashboard/dist/style.css'
+
 export default {
+  components: {
+    Dashboard
+  },
+  computed: {
+    uppy: () => new Uppy().use(XHRUpload, {
+      endpoint: '/upload',
+      fieldName: 'image',
+      headers: {
+          'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf_token"]').content
+      }
+    })
+  },
+  beforeDestroy () {
+    this.uppy.close({ reason: 'unmount' })
+  }
 }
 </script>
